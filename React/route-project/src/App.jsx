fb73e614473a677eb25ab313d0ec62478726ec2f
Kvsm-567sushma <susmitharathod57@gmail.com>
2024-10-27 23:04:23 +0530

    Update App.jsx

--- React/route-project/src/App.jsx
+++ React/route-project/src/App.jsx
@@ -3,17 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Product from './pages/Product'
 import Contact from './pages/Contact'
+import WebLayout from './layout/WebLayout'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
           <Routes>
+              <Route element={<WebLayout />}>
                <Route path='/' element= {<Home/>} />
                <Route path='/product' element={<Product/>}/>
                <Route path='/contact' element={<Contact/>}/>
           </Routes>
-
+       </Routes>
       </BrowserRouter>
     </>
   )
